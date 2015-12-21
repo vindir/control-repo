bf7eb51916b057f564ef9daf435cb1b5dbc881ff
Jamey Owens <jamey@jowens.org>
2015-12-21 05:39:59 -0500

    Change host_key to host_classes

--- site.pp
+++ site.pp
@@ -1,3 +1,3 @@
-$host_key = regsubst($fqdn, '\.', '_', 'G')
-notify { "Host Key Used for Classifier: $host_key": }
-hiera_include($host_key)
+$host_classes = regsubst($fqdn, '\.', '_', 'G')
+notify { "Host Key Used for Classifier: $host_classes": }
+hiera_include($host_classes)
